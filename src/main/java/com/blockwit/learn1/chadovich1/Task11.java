5cd63f7db8e2c5d20952d74940ca8d6237d74eee
chadovich1 <chadovich1@gmail.com>
2022-01-01 12:10:16 +0300

    Task11 test

--- src/main/java/com/blockwit/learn1/chadovich1/Task11.java
+++ src/main/java/com/blockwit/learn1/chadovich1/Task11.java
@@ -2,6 +2,6 @@ package com.blockwit.learn1.chadovich1;
 
 public class Task11 {
     public static void main (String[]  args )   {
-    System.out.println("Hello");
+    System.out.println("Hello world");
     }
 }
