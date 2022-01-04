e623afa947cd46bff967b6f2793049efdd44d744
chadovich1 <chadovich1@gmail.com>
2022-01-04 18:10:10 +0300

    Tasks4t1

---
+++ src/main/java/com/blockwit/learn1/chadovich1/Tasks4t1.java
@@ -0,0 +1,33 @@
+package com.blockwit.learn1.chadovich1;
+
+import java.util.Scanner;
+
+public class Tasks4t1 {
+  /* s4t1
+Написать программу, которая спрашивает у пользователя его год рождения и имя.
+ Затем вычисляет сколько ему лет и выводит в формате: "<имя>, вам <кол-во_лет> лет"
+  */
+
+    public static void main (String[]  args )   {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите ваш год рождения");
+        double yearbirth = in.nextDouble();
+        System.out.println("Введите ваше имя");
+        String uourName = in.nextLine();
+
+        int  uourAge ;
+        yearbirth = in.nextDouble();
+
+        String Name = uourName;
+        int Year = 2021;
+
+        int uourAge = (Year - yearbirth );
+
+
+
+        in.close();
+    }
+
+
+
+}
