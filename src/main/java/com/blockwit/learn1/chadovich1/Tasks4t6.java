4d0461ec61480df4cd8ed0c31989b3652afbd08b
chadovich1 <chadovich1@gmail.com>
2022-01-17 23:39:46 +0300

    17.01.22

--- src/main/java/com/blockwit/learn1/chadovich1/Tasks4t6.java
+++ src/main/java/com/blockwit/learn1/chadovich1/Tasks4t6.java
@@ -1,9 +1,8 @@
 package com.blockwit.learn1.chadovich1;
 
 //import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class Tasks4t6 {
   /* s4t6
 Написать программу, которая спрашивает у пользователя четыре числа.
@@ -11,34 +10,28 @@ public class Tasks4t6 {
   Затем выводит числа.
   */
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите четыре числа ");
-        int[] num = new int[4];
-        for (int i = 0; i < num.length; i++) {
-            num[i] = in.nextInt();
-            if (num[0] < num[num.length - 1]) {
-
-                int ind = num[0];
-                num[0] = num[num.length - 1];
-                num[num.length - 1] = ind;
-
-
-            }
-            else
-            {                 };
-            in.close();
+        public static void main(String[] args) {
+                Scanner in = new Scanner(System.in);
+                System.out.println("Введите четыре числа ");
+                int[] num = new int[4];
+                for (int i = 0; i < num.length; i++) {
+                        num[i] = in.nextInt();
+                }
+                if (num[0] < num[num.length - 1]) {
+                        int ind = num[0];
+                        num[0] = num[num.length - 1];
+                        num[num.length - 1] = ind;
+                }
+                for (int i = 0; i < num.length; i++){
+                                System.out.println(num[i]);
 
 
+                }
+                in.close();
         }
-
-
-    }
-
 }
 
-/*  for (int i = 0; i < num.length ; i++){
-           // for (int i = num .length-1; i >= 0; i--){
-            System.out.println(num[i]);
 
-        }*/
+
+
+
