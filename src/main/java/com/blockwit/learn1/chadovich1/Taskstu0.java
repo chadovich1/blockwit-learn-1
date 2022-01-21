4a86f6dd9ed760834b77adb0e4caa21c3623bd8e
chadovich1 <chadovich1@gmail.com>
2022-01-21 18:33:42 +0300

    21.01.22

--- src/main/java/com/blockwit/learn1/chadovich1/Taskstu0.java
+++ src/main/java/com/blockwit/learn1/chadovich1/Taskstu0.java
@@ -14,19 +14,20 @@ public class Taskstu0 {
 
         public static void main(String[] args){
                 int[] myArray = {12,7,89,-4,8,9};
-
-                for(int i = 0; i<6-1; i++) {
-                       // System.out.println("Выводим не сортированный массив" + myArray[i]);
-                        for (int j = i+1; j<myArray.length; j++) {
-                                System.out.println("Выводим не сортированный массив" + myArray[i]);
-                                if(myArray[i] > myArray[j]) {
-                                        int temp = myArray[i];
-                                        myArray[i] = myArray[j];
-                                        myArray[j] = temp;
+                System.out.println("Выводим не сортированный массив:");
+                for(int i = 0; i < myArray.length ; i++)
+                        System.out.println( myArray[i]);
+                System.out.println("Выводим сортированный массив:   " );
+                        for(int i = 0; i < 6; i++) {
+                                for (int j = i+1; j<myArray.length; j++) {
+                                        if(myArray[i] > myArray[j]) {
+                                           int temp = myArray[i];
+                                         myArray[i] = myArray[j];
+                                         myArray[j] = temp;
+                                        }
                                 }
+                                System.out.println( myArray[i]);
                         }
-                }
-                System.out.println(Arrays.toString(myArray));
         }
 
 }
